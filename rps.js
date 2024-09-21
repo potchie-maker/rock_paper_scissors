ec7acd9643d93570a45595286fb50a90e29fe934
potchie-maker <177373729+potchie-maker@users.noreply.github.com>
2024-09-21 13:10:46 -0400

    Start playRound()

--- rps.js
+++ rps.js
@@ -15,7 +15,6 @@ function getComputerChoice() {
 function getHumanChoice() {
   let choice = prompt("Make your choice!").toUpperCase();
   // console.log(choice);
-
   if (choice != "ROCK" && choice != "PAPER" && choice != "SCISSORS") {
     alert("Choice is invalid, please try again");
     getHumanChoice();
@@ -28,9 +27,26 @@ function getHumanChoice() {
 let humanScore = 0;
 let computerScore = 0;
 
-const humanSelection = getHumanChoice();
-// const computerSelection = getComputerChoice();
-
-
-function playRound(humanSelection, computerSelection) {
+function playRound(humanChoice, computerChoice) {
+  if (humanChoice === computerChoice) {
+    console.log("You tied. No winners here. Go another round.");
+  } else if (humanChoice === "ROCK" && computerChoice === "PAPER") {
+    console.log("You lost. Paper beats rock. Go another round.");
+    computerScore += 1;
+  } else if (humanChoice === "ROCK" && computerChoice === "SCISSORS") {
+    console.log("You WON!!! Rock beats scissors. Go another round.");
+    humanScore += 1;
+  } else if (humanChoice === "PAPER" && computerChoice === "ROCK") {
+    console.log("You WON!!! Paper beats rock. Go another round.");
+    humanScore += 1;
+  } else if (humanChoice === "PAPER" && computerChoice === "SCISSORS") {
+    console.log("You lost. Scissors beats paper. Go another round.");
+    computerScore += 1;
+  } else if (humanChoice === "SCISSORS" && computerChoice === "ROCK") {
+    console.log("You lost. Rock beats scissors. Go another round.");
+    computerScore += 1;
+  } else if (humanChoice === "SCISSORS" && computerChoice === "PAPER") {
+    console.log("You WON!!! Scissors beats paper. Go another round.");
+    humanScore += 1;
+  }
 }
