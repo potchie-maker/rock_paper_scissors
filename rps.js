8a1852f417fc2349371150be222cbaca7fec1760
potchie-maker <177373729+potchie-maker@users.noreply.github.com>
2024-09-21 11:31:51 -0400

    Finish getComputerChoice

--- rps.js
+++ rps.js
@@ -1,4 +1,13 @@
 function getComputerChoice() {
   let rand = Math.floor(Math.random() * 9) + 1;
+  let choice = "";
   console.log(rand);
+  if (rand >= 1 && rand <= 3) {
+    choice = "ROCK"
+  } else if (rand >= 4 && rand <= 6) {
+    choice = "PAPER"
+  } else if (rand >= 7 && rand <= 9) {
+    choice = "SCISSORS"
+  }
+  return choice;
 }
