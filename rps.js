fbcb962ce45ae554eff06f03a8c055f83d323db7
potchie-maker <177373729+potchie-maker@users.noreply.github.com>
2024-09-22 15:30:25 -0400

    Remove unnecessary comments and old code and add return statement in while loop for getHumanChoice()

--- rps.js
+++ rps.js
@@ -12,53 +12,13 @@ function getComputerChoice() {
   return choice;
 }
 
-// function getHumanChoice() {
-//   let choice = prompt("Make your choice!").toUpperCase();
-//   // console.log(choice);
-//   if (choice != "ROCK" && choice != "PAPER" && choice != "SCISSORS") {
-//     alert("Choice is invalid, please try again");
-//     // getHumanChoice();
-//   // } else {
-//     // console.log(choice);
-//     // return choice;
-//   }
-//   return choice;
-// }
-
-// function getHumanChoice() {
-//   // let choice = "";
-//   // while (choice === true) {
-//   // console.log(choice);
-//   if (prompt("Make your choice!").toUpperCase() != "ROCK" && prompt("Make your choice!").toUpperCase() != "PAPER" && prompt("Make your choice!").toUpperCase() != "SCISSORS") {
-//     alert("Choice is invalid, please try again");
-//   }
-//   return prompt("Make your choice!").toUpperCase();
-// }
-// }
-
-// function getHumanChoice() {
-//   let choice = prompt("Make your choice!").toUpperCase();
-//   let valid = false;
-//   // console.log(choice);
-//   while (valid) {
-//     if (choice != "ROCK" && choice != "PAPER" && choice != "SCISSORS") {
-//       alert("Choice is invalid, please try again");
-//       choice = choice;
-//     } else {
-//       valid === true;
-//     }
-//   }
-//   return choice;
-// }
-
 function getHumanChoice() {
   let choice = prompt("Make your choice!").toUpperCase();
-  while (choice != "ROCK" && choice != "PAPER" && choice != "SCISSORS") { // This works with "while" or "if" and I'm not sure why.
+  while (choice != "ROCK" && choice != "PAPER" && choice != "SCISSORS") {
     alert("Choice is invalid, please try again");
-    choice = getHumanChoice();
-    // console.log(choice); // This one adds more console logs if the string doesn't match the first time.
+    return getHumanChoice();
   }
-  // console.log(choice); // If the prompt is invalid the first time, it will log the same value twice when exiting the statement.
+  // console.log(choice);
   return choice;
 }
 
@@ -95,22 +55,15 @@ function playRound(humanChoice, computerChoice) {
 const humanSelection = getHumanChoice;
 const computerSelection = getComputerChoice;
 
-// playRound(humanSelection, computerSelection);
 
 function playGame() {
   let round = 1;
   let result = [];
-  // for (let i = 0; i < 5; i++) {
   while (round <= 5) {
-    // if (getHumanChoice === false) {
-    //   round = round;
-    // } else {
     console.log(`This is round ${round}`);
     result = playRound(humanSelection(), computerSelection());
     round++;
   }
-    // console.log(round);
-  // }
   console.log("------------------------------------------------------------");
   if (result[0] > result[1]) {
     console.log("YOU'VE WON THE MATCH!!!!! Reload the page to play again.");
