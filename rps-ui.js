d36430169c93cf32dc3b075703af265b2a423d92
potchie-maker <177373729+potchie-maker@users.noreply.github.com>
2024-12-08 22:53:17 -0500

    Finish assignment

--- rps-ui.js
+++ rps-ui.js
@@ -15,23 +15,52 @@ function getHumanChoice(callback) {
   document.querySelector(".scissors").addEventListener("click", () => callback(choices.scissors));
 }
 
+function updateRound(message) {
+  let round = document.querySelector(".round");
+  round.textContent = message;
+}
+
+function updateResult(message) {
+  let results = document.querySelector(".results");
+  results.textContent = message;
+}
+
+function updateScore(humanMessage, computerMessage) {
+  let humanScore = document.querySelector(".humanScore");
+  let computerScore = document.querySelector(".computerScore");
+  humanScore.textContent = humanMessage;
+  computerScore.textContent = computerMessage;
+}
+
+function updateWinner(message) {
+  let winner = document.querySelector(".winner");
+  winner.textContent = message;
+}
+
 function playRound(humanChoice, humanTotal, computerTotal) {
   let computerChoice = getComputerChoice();
   let humanScore = humanTotal;
   let computerScore = computerTotal;
+  let message = "";
 
   if (humanChoice === computerChoice) {
+    message = "You tied. No winners here. Go another round."
+    updateResult(message);
     console.log("You tied. No winners here. Go another round.");
   } else if (
     (humanChoice === "ROCK" && computerChoice === "PAPER") ||
-     (humanChoice === "PAPER" && computerChoice === "SCISSORS") ||
-     (humanChoice === "SCISSORS" && computerChoice === "ROCK")
+    (humanChoice === "PAPER" && computerChoice === "SCISSORS") ||
+    (humanChoice === "SCISSORS" && computerChoice === "ROCK")
   ) {
-     console.log(`You lost. ${computerChoice} beats ${humanChoice}. Go another round.`);
-     computerScore++;
+    message = `You lost. ${computerChoice} beats ${humanChoice}. Go another round.`
+    updateResult(message);
+    console.log(`You lost. ${computerChoice} beats ${humanChoice}. Go another round.`);
+    computerScore++;
   } else {
+    message = `You WON!!! ${humanChoice} beats ${computerChoice}. Go another round.`
+    updateResult(message);
     console.log(`You WON!!! ${humanChoice} beats ${computerChoice}. Go another round.`);
-     humanScore++;
+    humanScore++;
   } 
    return [humanScore, computerScore];
 }
@@ -40,13 +69,23 @@ function playGame() {
   let result = [0,0];
   let round = 1;
   const winLimit = 5;
+  let winnerMessage = "";
+  let roundMessage = "";
+  let humanScoreMessage = "";
+  let computerScoreMessage = "";
 
   function endGame() {
     if (result[0] === result[1]) {
+      winnerMessage = "The match ended in a tie. Reload the page to play again.";
+      updateWinner(winnerMessage);
       console.log("The match ended in a tie. Reload the page to play again.");
     } else if (result[0] === winLimit) {
+      winnerMessage = "YOU'VE WON THE MATCH!!!!! Reload the page to play again.";
+      updateWinner(winnerMessage);
       console.log("YOU'VE WON THE MATCH!!!!! Reload the page to play again.");
     } else if (result[1] === winLimit) {
+      winnerMessage = "You lost the match. Reload the page to play again.";
+      updateWinner(winnerMessage);
       console.log("You lost the match. Reload the page to play again.");
     }
 
@@ -58,8 +97,13 @@ function playGame() {
   
   getHumanChoice((humanChoice) => {;
     if (result[0] < winLimit && result[1] < winLimit) {
-      console.log(`This is round ${round}`);
+      roundMessage = `Round ${round}`
+      updateRound(roundMessage);
+      console.log(`Round ${round}`);
       result = playRound(humanChoice, result[0], result[1]);
+      humanScoreMessage = `YOU: ${result[0]}`;
+      computerScoreMessage = `Computer: ${result[1]}`;
+      updateScore(humanScoreMessage, computerScoreMessage);
       console.log(result);
       round++;
 
